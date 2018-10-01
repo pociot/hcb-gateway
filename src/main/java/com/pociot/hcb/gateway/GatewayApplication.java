ce34cbce3b8e6d8e50c2ac8d4f97ffb18d70d1f0
kamilpociot <kamil.pociot@me.com>
2018-10-01 16:26:10 +0200

    oauth2 workaround

--- src/main/java/com/pociot/hcb/gateway/GatewayApplication.java
+++ src/main/java/com/pociot/hcb/gateway/GatewayApplication.java
@@ -3,6 +3,11 @@ package com.pociot.hcb.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +15,15 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter() {
+			@Override
+			public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+				return chain.filter(exchange);
+			}
+		};
 	}
 }
